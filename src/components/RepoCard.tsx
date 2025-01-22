90021fb33a527961ab3baf01e0740fe8496c7387
Roberto Nazareth <nazarethguedesroberto@gmail.com>
2025-01-22 00:11:16 -0300

    fix: ajustes nos layouts dos cards

--- src/components/RepoCard.tsx
+++ src/components/RepoCard.tsx
@@ -16,25 +16,21 @@ export function RepoCard(props: Props) {
                 "w-full md:w-[310px] h-[200px] bg-slate-700 hover:bg-slate-500 transition-colors"
             ])}
         >
+            <div className="flex flex-col items-center gap-2">
+                <img
+                    src={props.avatar}
+                    className="rounded-full w-[50px] h-[50px] aspect-square object-cover"
+                    alt={`Avatar de ${props.ownerName}`}
+                />
+            </div>
+
             <div className="flex flex-1 flex-col gap-5">
-                <h2 className="text-white text-2xl">
+                <h2 className="text-white text-xl">
                     {props.name}
                 </h2>
 
                 <p className="text-white max-w-[250px]">
                     {props.description?.slice(0, 60)}
-                </p>
-            </div>
-
-            <div className="flex flex-col items-center gap-2">
-                <img
-                    src={props.avatar}
-                    className="rounded-full w-[60px] h-[60px] aspect-square object-cover"
-                    alt={`Avatar de ${props.ownerName}`}
-                />
-
-                <p className="text-white">
-                    {props.ownerName}
                 </p>
             </div>
         </div>
